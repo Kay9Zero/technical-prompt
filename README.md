301dae86a10eb4a489228b9ebea0460c29f6a088
Kay9Zero <Kay9Zero@users.noreply.github.com>
2021-04-15 21:19:45 -0600

    feat: Use Carton and Try::Tiny
    Conceiving an idea to throw an exception inside the sort code block
    while checking values but then catch that outside and return the
    expected null value in those cases.
    
    It's best to avoid using eval and to instead use Try::Tiny for something
    like this. Since that is a third party library though, Carton can
    facilitate managing our dependencies.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Claravine Technical Prompt
+
+## Dependencies
+
+Using [Carton](https://metacpan.org/pod/Carton) to manage CPAN libraries
+
+## Installing
+
+  > carton install
+
+## Running/Testing
+
+  > carton exec -- prove -v -l lib t
--- lib/TechnicalPrompt.pm
+++ lib/TechnicalPrompt.pm
@@ -5,22 +5,18 @@ use warnings;
 use Exporter 'import';
 our @EXPORT_OK = qw(findPair);
 
+use Try::Tiny;
+
 sub findPair {
     my ($integers, $desired_sum) = @_;
 
-    # TODO Determine to validate first, sort first, or do both simultaneously.
-    #      The later has the benifit of less processing cycles but at the cost
-    #      of complicating code maintainability.
-
-    # TODO Validate input, return undef (aka null outside of perl) upon finding
-    #      any invalid parameters
-
-    # TODO Sort input into ascending order
+    # TODO While sorting validate input. Upon finding invalid value throw exception
+    #      where in catch return undef (aka null outside of perl)
 
     # TODO Iterate over the sorted list, have an inner loop over the remaining
     #      siblings where it sums the pairs until the desired sum is matched
 
-    return [0, 0]
+    return [0, 0];
 }
 
 1;
