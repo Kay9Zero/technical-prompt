1f728e216ee35a5ec230303d8d88e91c51814d11
Kay9Zero <Kay9Zero@users.noreply.github.com>
2021-04-15 21:27:53 -0600

    feat: Implement validation and sorting logic
    This will get half of the tests passing.

--- lib/TechnicalPrompt.pm
+++ lib/TechnicalPrompt.pm
@@ -2,6 +2,7 @@ package TechnicalPrompt;
 use strict;
 use warnings;
 
+use Scalar::Util qw(looks_like_number);
 use Exporter 'import';
 our @EXPORT_OK = qw(findPair);
 
@@ -10,8 +11,22 @@ use Try::Tiny;
 sub findPair {
     my ($integers, $desired_sum) = @_;
 
-    # TODO While sorting validate input. Upon finding invalid value throw exception
-    #      where in catch return undef (aka null outside of perl)
+    $integers = try {
+        my $i = 0;
+        [
+            sort {
+                die "Invalid element\n" unless looks_like_number($a) && $a > 0 && $a != $b;
+                $a <=> $b
+            } @$integers
+        ];
+    }
+    catch {
+        my $err = $_;
+        return undef if $err eq "Invalid element\n";
+        die $err;
+    };
+
+    return undef unless defined $integers;
 
     # TODO Iterate over the sorted list, have an inner loop over the remaining
     #      siblings where it sums the pairs until the desired sum is matched
